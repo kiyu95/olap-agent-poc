267eff07a4d29bbd7251d973595e7dffb43ac59c
kiyu95 <giu0428@gmail.com>
2026-04-08 13:43:58 +0900

    feat: create project

---
+++ src/main/java/com/example/olapagentpoc/OlapAgentPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.olapagentpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlapAgentPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlapAgentPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/olapagentpoc/OlapAgentPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.olapagentpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlapAgentPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
